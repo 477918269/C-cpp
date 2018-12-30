3d57a8684f6f26cdd46abdc5e4302e80bbb04bf9
477918269 <477918269@qq.com>
2018-12-30 21:23:31 +0800

    C的分支和循环

---
+++ 12.30/12.30/test.c
@@ -0,0 +1,156 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+
+//int main() 
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	printf("%d\n", sizeof(long double));
+//	system("pause");
+//	return 0;
+//}
+int main()
+{
+	//int a = 3;
+	//float b = 3.1415926;
+	//float c = 5.414;
+
+	//printf("%d\n", a);
+	//printf("%f\n", a);
+	//printf("%d\n", b);
+	//printf("%f\n", b);
+	//printf("%d\n", c);
+	//printf("%f\n", c);
+
+	//system("pause");
+
+	//return 0;
+	//int num1 = 0;
+	//int num2 = 0;
+	//int sum = 0;
+	//printf("输入两个操作数:>");
+	//scanf("%d%d", &num1, &num2);
+	//sum = num1 + num2;
+	//printf("%d", sum);
+
+	//system("pause");
+	/*printf("");
+	system("pause");*/
+	/*int num = 0;
+	for (num = 0; num <= 100; num++)
+	{
+		if (num % 2 == 1)
+		{
+			printf("%d\n", num);
+		}
+	}*/
+	//int n = 1;
+	//int m = 2;
+	//switch (n)
+	//{
+	//	case 1:
+	//		m++;
+	//	case 2:
+	//		n++;
+	//	case 3:
+	//		switch (n)
+	//		{
+	//			//switch允许嵌套使用
+	//		case 1:
+	//			n++;
+	//		case 2:
+	//			m++;
+	//			n++;
+	//			break;
+	//		}
+	//	case 4:
+	//		m++;
+	//		break;
+	//	default:
+	//		break;
+
+	//}
+	//printf("m = %d, n = %d\n", m, n);
+
+	//system("pause");
+	//int i = 0;
+	//int j = 0;
+	//for (i = 0, j = 0; j = 0; j++, i++)
+	//{
+	//	j++;
+	//}
+	//printf("%d", j);
+	//char a1[] = "welcome to bit";
+	//char a2[] = "##############";
+	//int left = 0;
+	//int right = strlen(a1) - 1;
+	//printf("%s\n", a2);
+	//while (left <= right)
+	//{
+	//	char tmp = a1[left];
+	//	a1[left] = a1[right];
+	//	a1[right] = tmp;
+	//	left++;
+	//	right--;
+	//	printf("%s\n",a1);
+	//}
+
+
+	/*
+	int i = 0;
+	int put = 0;
+	int j = 6;
+	while (i < 3)
+	{
+		printf("答案在1-10之间，请输入你猜的数字,共有三次机会\n");
+		scanf("%d", &put);
+		if (put == j)
+		{
+			break;
+		}
+		i++;
+		printf("输入错误\n");
+		
+	}
+	if (i == 3)
+		printf("失败\n");
+	else
+		printf("成功\n");*/
+	//int i = 0;
+	//int sum = 1;
+	//printf("请输入你所求阶乘的最大项");
+	//scanf("%d", &i);
+	//while (i>=1)
+	//{
+	//	sum = sum * i;
+	//	i--;
+	//}
+	//printf("%d", sum);
+	//int i = 0;
+	//int	j = 0;
+	//int all = 0;
+	//for (i = 1; i <= 10; i++)
+	//{
+	//	int sum = 1;
+	//	for (j = i; j >= 1; j--)
+	//	{
+	//		sum = sum * i;
+	//	}
+	//	
+	//	all = all + sum;
+	//}
+	//printf("%d", all);
+
+	
+	
+
+//
+//	system("pause");
+//	
+//	return 0;
+//}
