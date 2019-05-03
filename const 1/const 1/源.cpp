395437a2c8209007f2a9370848e8f9d780a034af
477918269 <477918269@qq.com>
2019-05-03 18:21:50 +0800

    const

---
+++ const 1/const 1/源.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+class Stack
+{
+private:
+	int ad;
+public:
+	void Push(int elem);
+	
+	int GetCount(void) const;
+};
+
+int Stack::GetCount() const;
+{
+	++ad;
+	Push();
+	return 
+}
+
+int main()
+{
+
+}
