e2fdea4c72574a7d9aea63ace171e1ff46dbeee9
477918269 <477918269@qq.com>
2019-05-21 20:22:00 +0800

    格式输出

---
+++ 百分号的输出/百分号的输出/源.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	//int count = 110;
+	//printf("%d\n", count);
+	//printf("%5d\n", count);
+	//printf("%3d\n", count);
+	float f = 3.1415926;
+	printf("%f\n", f);
+	printf("%6.5f\n", f);
+	printf("%3.5f\n", f);
+	printf("%8.3f\n", f);
+	printf("%-8.3f\n", f);
+	printf("%2.8f\n", f);
+
+
+	//char* str = "Helloworld";
+	//printf("%s\n", str);
+	//printf("%5.3s\n", str);
+	//printf("%-5.3s\n", str);
+	//printf("%5.6s\n", str);
+	//printf("%-5.6s\n", str);
+	system("pause");
+	return 0;
+}
