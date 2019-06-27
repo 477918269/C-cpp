b3b568bc55fb7d98c533ebaf51819d7f247b6b97
477918269 <477918269@qq.com>
2019-06-27 22:51:23 +0800

    C++IO流

---
+++ C和C++的IO流/C和C++的IO流/源.cpp
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+////int main()
+////{
+////	int a = 100;
+////	FILE* pf = fopen("test.txt", "wb");
+////	fwrite(&a, 4, 1, pf);
+////	fclose(pf);
+////	pf = NULL;
+////	system("pause");	
+////	return 0;
+////}
+//int main() 
+//{
+//	FILE * pFile;
+//	pFile = fopen("myfile.txt", "w");
+//	if (pFile != NULL)
+//	{
+//		fputs("fopen example", pFile);
+//		fclose(pFile);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
