9990b1b74eb99c4a2a9a12c6eab9d4c71d10c6a4
用户名 <>
2019-10-09 23:19:13 +0800

    文件操作

---
+++ C文件操作/C文件操作/源.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+int main()
+{
+	FILE* p;
+	//p = fopen("test.txt", "r");
+	//if (p != NULL)
+	//{
+	//	fclose(p);
+	//}
+
+	//p = fopen("test.txt", "r");
+	//int ch = 0;
+	//ch = printf("%c\n", ch);
+	//fclose(p);
+	//p = fopen("test.txt", "w");
+	//fputc('A', p);
+	//fputc('L', p);
+	//fputc('A', p);
+	//fclose(p);
+	//p = NULL;
+	//p = fopen("test.txt", "w");
+	//int ch = 0;
+	//if (p == NULL)
+	//{
+	//	printf("%s\n", strerror(errno));
+	//	return 0;
+	//}
+	//fputs("hellow bitch", p);
+	//fclose(p);
+	//p = NULL;
+
+	//p = fopen("test.txt", "r");
+	//char arr[20] = {0};
+	//fgets(arr, 20, p);
+	//printf("%s", arr);
+	//fclose(p);
+	//p = NULL;
+
+
+
+	system("pause");
+	return 0;
+}
