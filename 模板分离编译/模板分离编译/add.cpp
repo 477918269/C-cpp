a2a1a435fe030bfc76ef8e960949aacbc84edf6c
用户名 <>
2019-10-13 23:05:05 +0800

    模板分离编译

---
+++ 模板分离编译/模板分离编译/add.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+using namespace std;
+
+template<class T>
+T ADD(const T& left, const T& right)
+{ return left + right; }
---
+++ 模板分离编译/模板分离编译/main.cpp
@@ -0,0 +1,9 @@
+#include"Add.h"
+
+
+int main()
+{
+	ADD(1, 2); 
+	ADD(1.0, 2.0);
+	return 0;
+}
